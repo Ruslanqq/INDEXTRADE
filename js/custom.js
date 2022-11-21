b3f9b1eddcc08e1897f0f9c26f62251ebbeaec15
Ruslanqq <66884681+Ruslanqq@users.noreply.github.com>
2022-11-21 17:31:08 +0600

    first commitйцуйцу

--- js/custom.js
+++ js/custom.js
@@ -34,8 +34,8 @@ $(document).ready(function() {
     $('body').vegas({
         slides: [
             { src: 'images/slide-img1.jpg' },
-            { src: 'images/slide-img2.jpg' },
-            { src: 'images/slide-img3.jpg' }
+            { src: 'images/slide-img1.jpg' },
+            { src: 'images/slide-img1.jpg' }
         ],
         timer: false,
         transition: [ 'zoomIn', ],
